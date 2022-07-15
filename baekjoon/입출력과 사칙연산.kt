9fa993fd936479a52d11028e4b2cf0d399983b43
Ahyun <99587191+Ahyun-J@users.noreply.github.com>
2022-07-15 23:41:48 +0900

    Create 입출력과 사칙연산.kt

---
+++ baekjoon/입출력과 사칙연산.kt
@@ -0,0 +1,109 @@
+// 1 Hello World
+
+fun main() {
+    println("Hello World!")
+}
+
+//---------------------
+// 2 We love kriii
+fun main(){
+    var str = "강한친구 대한육군"
+    print("$str\n$str")
+}
+
+//---------------------
+// 3 고양이
+fun main(){
+    println("""\    /\
+ )  ( ')
+(  /  )
+ \(__)|""")
+}
+
+//---------------------
+// 4 개
+
+//---------------------
+// 5 A+B
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var a = scanner.nextInt()
+    var b = scanner.nextInt()
+    println(a+b)
+}
+
+//---------------------
+// 6 A-B
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var a = scanner.nextInt()
+    var b = scanner.nextInt()
+    println(a-b)
+}
+
+//---------------------
+// 7 A*B
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var a = scanner.nextInt()
+    var b = scanner.nextInt()
+    println(a*b)
+}
+
+//---------------------
+// 8 A/B
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var a = scanner.nextDouble()
+    var b = scanner.nextInt()
+    println(a/b)
+}
+
+//---------------------
+// 9 사칙연산
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var a = scanner.nextInt()
+    var b = scanner.nextInt()
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+}
+
+//---------------------
+// 10 ??!
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val s: String = scanner.nextLine()
+    println("$s??!")
+}
+
+//---------------------
+// 11 1998년생인 내가 태국에서는 2541년생?!
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var a = scanner.nextInt()
+    println(a - 543)
+}
+
+//---------------------
+// 12 나머지
+
+//---------------------
+// 13 곱셈
