f6469ce147ef29f6a314a5cf615cdfe82f2c9865
Ahyun <99587191+Ahyun-J@users.noreply.github.com>
2022-07-24 00:05:55 +0900

    Update 3주차.kt

--- baekjoon/3주차.kt
+++ baekjoon/3주차.kt
@@ -96,7 +96,7 @@ fun main(){
 }
 
 // 1차원 배열 ------------------
-// 6. OX퀴즈 (8958)
+//? 6. OX퀴즈 (8958)
 import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
@@ -118,4 +118,50 @@ fun main() = with(Scanner(System.`in`)) {
 }
 
 //------------------
-// 7. 평균은 넘겠지 (4344)
+//? 7. 평균은 넘겠지 (4344)
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val num = nextInt()
+
+    repeat(num) {
+        val number = nextInt()
+        val array = IntArray(number)
+
+        for (i in array.indices) {
+            array[i] = nextInt()
+        }
+
+        val total = totalScore(array)
+        val average = average(total, array.size)
+        val overPeople = overPeople(array, average)
+        overPeopleAverage(overPeople, array.size)
+    }
+}
+
+private fun totalScore(array: IntArray): Int {
+    var total = 0
+
+    for (i in array.indices) {
+        total += array[i]
+    }
+
+    return total
+}
+
+private fun average(total: Int, peoples: Int) = total / peoples
+private fun overPeople(array: IntArray, averageScore: Int): Int {
+    var overPeople = 0
+    for (i in array.indices) {
+        if (array[i] > averageScore) {
+            overPeople++
+        }
+    }
+
+    return overPeople
+}
+
+private fun overPeopleAverage(overPeople: Int, allPeople: Int) {
+    val average = (overPeople.toDouble() / allPeople.toDouble()) * 100
+    println(String.format("%.3f%%", average))
+}
